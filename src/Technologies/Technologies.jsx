a2dac178db0db608ebf1a6d2c3697f480bd32476
Test User <testuser@MacBook-Pro-2.local>
2022-08-25 13:54:28 -0400

    adding styling to tech portion

--- src/Technologies/Technologies.jsx
+++ src/Technologies/Technologies.jsx
@@ -6,22 +6,23 @@ import react from "../pictures/react1.png"
 import tw from "../pictures/tailwind.png"
 import mui from "../pictures/mui.png"
 import node from "../pictures/node.png"
+import './Technologies.css'
 
 
 const Technologies = () => {
-  return (
-    <div>
-        <div className="flex flex-row">
-      <img width={100} src={css} alt="css"/>
-      <img width={100} src={html} alt="css"/>
-      <img width={100} src={js} alt="css"/>
-      <img width={100} src={react} alt="css"/>
-      <img width={100} src={mui} alt="css"/>
-      <img width={100} src={tw} alt="css"/>
-      <img width={100} src={node} alt="css"/>
-      </div>
-    </div>
-  )
+    return (
+        <div className='container'>
+            <div className="tech-card">
+                <img width={100} src={css} alt="css" />
+                <img width={100} src={html} alt="css" />
+                <img width={100} src={js} alt="css" />
+                <img width={100} src={react} alt="css" />
+                <img width={100} src={mui} alt="css" />
+                <img width={100} src={tw} alt="css" />
+                <img width={100} src={node} alt="css" />
+            </div>
+        </div>
+    )
 }
 
 export default Technologies

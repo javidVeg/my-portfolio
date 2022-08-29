8a548efbf365cb98bb5e782675d9243cd6735e63
Test User <testuser@MacBook-Pro-2.local>
2022-08-29 07:37:53 -0400

    footer edits

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,18 +1,48 @@
 import React from 'react';
+import memoji from '../pictures/david5.png'
+import { AiOutlineLinkedin } from "react-icons/ai"
+import { FiGithub } from "react-icons/fi"
+import { FiTwitter } from "react-icons/fi"
 import "./Footer.css";
 
 const Footer = () => {
-  return (
-    <div className='footer'>
-        <div className='footer-container'>
-        <div className='grid-item-1'>Navigation</div>
-        <div className='grid-item-2'>Contact</div>
-        <div className='grid-item-3'>Emoji</div>
-        <div className='grid-item-4'>Copyright 2022 Copyright 2022 Copyright 2022 Copyright 2022 Copyright 2022</div>
-        </div>
+    return (
+        <div className='footer'>
+            <div className='footer-container'>
+                <div className='grid-item-1 text-center mt-10 text-white '>
+                    <div>
+                        <h1>Navigation</h1>
+                        <hr className='divider' />
+                    </div>
+                    <p>Home</p>
+                    <p>Technologies</p>
+                    <p>Projects</p>
+                    <p>Contact</p>
+                </div>
+                <div className='grid-item-2 mt-10 flex flex-col items-center text-white'>
+                    <div className='flex flex-col items-center'>
+                        <h1>Find Me @</h1>
+                        <hr className='divider' />
+                    </div>
+                    <AiOutlineLinkedin color="#2563EB" size={30} className='mt-3' />
+                    <FiGithub size={30} color="#A855F7" className='mt-3' />
+                    <FiTwitter size={30} color="#93C5FD" className='twitter mt-3' />
+                </div>
+                <div className='grid-item-3'>
+                    <img src={memoji} alt="alt-memoji" width="250vw" />
+                </div>
+                <div className='grid-item-4 text-white flex flex-row flex-wrap justify-center '>
+                    <div className='foot-left text-emerald-500'>
+                        David Andrew Martinez &reg; {new Date().getFullYear()}
+                    </div>
+                    <div className='foot-right' >
+                        Not created on Wordpress because thats lame..
+                    </div>
+                </div>
+            </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Footer

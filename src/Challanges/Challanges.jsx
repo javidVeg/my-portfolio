debdc7a5d3195b1d4a335fc38cbe783aa84548d8
David_Martinez <javidx24@icloud.com>
2022-11-22 08:06:45 -0500

    codewars edits

--- src/Challanges/Challanges.jsx
+++ src/Challanges/Challanges.jsx
@@ -8,15 +8,27 @@ const Challanges = () => {
     const [challanges, setChallanges] = useState([])
     // const [challangesSlug, setChallangesSlug] = useState([])
 
-    let array = [];
+    let codewarsData = [
+        {
+        "Title": "DNA to RNA Conversion",
+        "Date": "2022-09-19T22:36:40.147Z",
+        "Description": "Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T'). \n\nRibonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').\n\nCreate a function which translates a given DNA string into RNA.\n\nFor example:\n\n```\n\"GCAT\"  =>  \"GCAU\"\n```\n\nThe input string can be of arbitrary length - in particular, it may be empty.  All input is guaranteed to be valid, i.e. each input string will only ever consist of `'G'`, `'C'`, `'A'` and/or `'T'`."
+    },
+    {
+        "Title": "If you can't sleep, just count sheep!!",
+        "Date": "2022-11-06T22:10:07.558Z",
+        "Description": "If you can't sleep, just count sheep!!\n\n## Task:\nGiven a non-negative integer, `3` for example, return a string with a murmur: `\"1 sheep...2 sheep...3 sheep...\"`.  Input will always be valid, i.e. no negative integers.\n"
+    }
+];
 
     useEffect(() => {
-
+        //! THIS GETS DATA SET FROM CODEWARS API TO INCLUDE THE ~SLUG~ USED TO GET  
+        //! EXTRA DATA USED BELOW.
         const challange = () => {
             Axios.get("https://www.codewars.com/api/v1/users/javidVeg/code-challenges/completed?page=0")
                 .then((response) => {
                     setChallanges(response.data.data)
-   
+
                 });
         };
         challange();
@@ -26,25 +38,27 @@ const Challanges = () => {
 
     }, [])
 
-    console.log(challanges)
-
-    const slugs = () => {
+    //! THIS FUNCTION GETS REMAINING DATA BASED ON USEEFFECT GET REQUEST AND PUSHES
+    //! IT INSIDE THE EMPTY ARRAY VARIABLE.
+    const extraAPIData = () => {
         challanges.forEach((element, i) => {
-            console.log(element.slug)
-           
             Axios.get(`https://www.codewars.com/api/v1/code-challenges/${element.slug}`)
-            .then((response) => {
-                array.push(response.data.name)
-                array.sort(function(a,b){ return b-a; })
-                // console.log(challangesSlug)
-                
-
-            })
+                .then((response) => {
+                    codewarsData.push({
+                        "Title": response.data.name,
+                        "Date": element.completedAt,
+                        // "Language": element.completedLanguages,
+                        "Description": response.data.description
+                    })
+                })
         });
     }
-    slugs();
+    extraAPIData();
 
-    console.log(array)
+    
+
+    console.log(codewarsData)
+    console.log(challanges)
 
     return (
         <div className="challanges-container">
@@ -55,9 +69,22 @@ const Challanges = () => {
                 <h3 className="codewars-title text-emerald-500">Completed Codewars Challanges</h3>
                 <a href="https://www.codewars.com/users/javidVeg"><img src="https://www.codewars.com/users/javidVeg/badges/large"></img></a>
             </div>
-            <div className="completed">
+            <p className="completed">
+                
+                {
+                    
+                    challanges.map(({ name }) =>
+                        (
+                            <>
+                                <p>{name}</p>
+                               
+                            </>
 
-            </div>
+                    )           
+
+                    )
+                }
+            </p>
         </div>
     )
 }

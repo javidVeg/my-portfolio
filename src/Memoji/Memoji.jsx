d21b4e6945067e1caeae8bbadd64d3d33bd472a1
David_Martinez <javidx24@icloud.com>
2022-11-26 17:07:30 -0500

    added gradient to name

--- src/Memoji/Memoji.jsx
+++ src/Memoji/Memoji.jsx
@@ -16,12 +16,17 @@ const Memoji = () => {
             </div>
             <div className='name grid place-items-center '>
                 <h1>
-                    <span className=' text-slate-500'>&lt;</span><span className=' text-emerald-500'>DavidMartinez</span><span className=' text-slate-500'>/&gt;</span>
+                    
+                        <span className=' text-slate-500'>&lt;</span>
+                        <span className='david-span'>DavidMartinez</span>
+                        <span className=' text-slate-500'>/&gt;</span>
+                    
+                 
                 </h1>
             </div>
             <div className='role -mt-2 ml-6 md:ml-12  grid grid-rows-2 lg:ml-24 '>
                 <h1>
-                    Front-End Developer
+                    Frontend Developer
                 </h1>
                 <p className=' text-slate-500 '>
                     Los Angeles Native Living in the <span><a className='p-1 border border-neutral-700 rounded-sm' href='https://maps.apple.com/?address=Potomac%20River,%20Washington,%20DC,%20United%20States&auid=5294854946377719338&ll=38.884414,-77.052401&lsp=7618&q=Dropped%20Pin&_ext=EiYp2gJC6+HxQkAxxC+rZgxuU8A5VDsejgmqQ0BBJL0DlyIPU8BQDA%3D%3D'>DMV</a></span>
